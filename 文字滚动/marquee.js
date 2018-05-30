1b13276b75e16164eb19cd8c7d9dbf10ea78af5a
Trevor <trevor0426@outlook.com>
2018-05-30 16:09:35 +0800

    文字滚动

---
+++ 文字滚动/marquee.js
@@ -0,0 +1,28 @@
+function marquee(options){ 
+	if(!options){ 
+		throw new Error("Missing parameter"); 
+	return false; 
+	} 
+	var id = options.id, 
+		delay = options.delay || '0.6s', 
+		timingFunction = options.timingFunction || 'ease', 
+		interval = options.interval || 1000; 
+	if(!id){ 
+		throw new Error("Missing id"); 
+		return false; 
+	} 
+	var element = document.getElementById(id); 
+	var element_height = element.clientHeight; 
+	var timer = setInterval(function(){ 
+		fc = element.children[0]; 
+		cn = fc.cloneNode(true); 
+		element.appendChild(cn); 
+		element.style.transition = 'top '+ delay + ' ' + timingFunction; 
+		element.style.top = '-'+ element_height +'px'; 
+		setTimeout(function(){ 
+			element.removeChild(fc); 
+			element.style.transition = 'none'; 
+			element.style.top = '0px'; 
+		},800); 
+	},interval); 
+} 
